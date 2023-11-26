810f5cfc9f5a5078063eb60a83f21d5566d3de60
root <root@DESKTOP-RS21C02.localdomain>
2023-11-26 16:33:24 +0900

    色々変えた

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="@/assets/logo.png" alt="Logo" class="logo">
+    <HelloWorld msg="株式会社robottteへようこそ"/>
+    <p>すばらしい！</p>
   </div>
 </template>
 
@@ -24,5 +25,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+/* 基本のスタイル */
+.logo {
+    width: 60vw; /* ビューポートの横幅の60% */
+    height: auto; /* 縦横比を保つ */
+}
+/* スマートフォン用のスタイル（画面幅が600px以下の場合） */
+@media (max-width: 600px) {
+    .logo {
+        width: 90vw; /* ビューポートの横幅の90% */
+    }
 }
 </style>
