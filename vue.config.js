048c75d9ea36020343d9ec839961c765ad54cb03
root <root@DESKTOP-RS21C02.localdomain>
2023-11-26 16:44:47 +0900

    やり直し

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/robottte/' // プロジェクト名に置き換えてください
+  : '/'
 })
