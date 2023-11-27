fc7f786e52d19890c0e4b269bd17954011f16853
root <root@DESKTOP-RS21C02.localdomain>
2023-11-27 14:36:46 +0900

    map

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,12 @@ import VueRouter from 'vue-router';
 import CompanyOverview from '@/views/CompanyOverview.vue'; // 各ページに対応するコンポーネントをインポート
 import AccessPoint from '@/views/AccessPoint.vue';
 import ProductIntroduction from '@/views/ProductIntroduction.vue';
-import App from '@/App.vue'; // App.vue ファイルのパスを適切に指定する
+// import App from '@/App.vue'; // App.vue ファイルのパスを適切に指定する
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: App }, // メインページは App.vue
+//  { path: '/', component: App }, // メインページは App.vue
   { path: '/accesspoint', component: AccessPoint },
   { path: '/productintroduction', component: ProductIntroduction },
   { path: '/companyoverview', component: CompanyOverview },
