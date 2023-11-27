14eed8f67d86ea260c3463de148bd2cda291a549
root <root@DESKTOP-RS21C02.localdomain>
2023-11-27 15:49:03 +0900

    map2

--- src/views/AccessPoint.vue
+++ src/views/AccessPoint.vue
@@ -1,4 +1,4 @@
-<!-- src/views/CompanyOverview.vue -->
+<!-- src/views/AccessPoint.vue -->
 <template>
 <div class="fade-in">
   <div class="accesspoint">
@@ -10,9 +10,11 @@
       height="450"
       frameborder="0"
       style="border:0"
-      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDhxZaNA03BvbeW8Pq33hdEr29z1XvhIuI&q=東京都練馬区北町２-21-10"
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBYVQbNeQq8Q7w2XIsyK0nW3tesOtV93_A&q=東京都練馬区北町２-21-10"
       allowfullscreen
     ></iframe>
+ 
+
   </div>
 </div>
 </template>
@@ -25,6 +27,7 @@ export default {
     };
   },
 };
+
 </script>
 
 <style scoped>
