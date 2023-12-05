593db853b4b717058b970a49a485797c8b40ab9c
root <root@DESKTOP-RS21C02.localdomain>
2023-12-05 19:37:36 +0900

    aa

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,21 @@
-const { defineConfig } = require('@vue/cli-service') 
+const { defineConfig } = require('@vue/cli-service');
 
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = defineConfig((context) => {
+  // NODE_ENV 環境変数を取得
+  const isProduction = process.env.NODE_ENV === 'production';
 
-  devServer: {
-    port: 3000  
+  // 本番リモート環境用の設定
+  if (isProduction) {
+    return {
+      transpileDependencies: true,
+      publicPath: '/robottte/' // プロジェクト名に置き換えてください
+    };
+  } else { // ローカル環境用の設定
+    return {
+      transpileDependencies: true,
+      devServer: {
+        port: 3000 // ローカル環境のポート番号を指定
+      }
+    };
   }
-})
+});
